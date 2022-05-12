e58829a350b6a7c5be2a1dc2f676b1d457807075
Mirian Santos <mirian.trsantos@gmail.com>
2022-05-12 19:30:35 -0300

    projeto inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,29 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function unmute() {
+    var video = document.getElementById('trailer1');
+    if (video.muted) {
+        video.muted = false;
+        $('#icone-som').removeClass("fa-solid fa-volume-xmark");
+        $('#icone-som').addClass("fa-solid fa-volume-high");
+    } else {
+        video.muted = true;
+        $('#icone-som').removeClass("fa-solid fa-volume-high");
+        $('#icone-som').addClass("fa-solid fa-volume-xmark");
+    }
+}
